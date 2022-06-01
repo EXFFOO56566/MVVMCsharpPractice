9e8bdaeb50e56ddd7adcca5b7e62cd21a56270e2
tochyodikwa <odikwatochy233@gmail.com>
2022-06-01 07:33:08 -0700

    first commit

---
+++ Views/StartPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using SampleUniversalApp.Models;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SampleUniversalApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Messenger.Default.Register<ShowMessageDialog>
+            (
+                 this,
+                 (action) => ReceiveMessage(action)
+            );
+        }
+
+        private async void ReceiveMessage(ShowMessageDialog action)
+        {
+            DialogService dialogService= new DialogService();
+            await dialogService.ShowMessage(action.Message, "Sample Universal App");
+        }
+    }
+}
